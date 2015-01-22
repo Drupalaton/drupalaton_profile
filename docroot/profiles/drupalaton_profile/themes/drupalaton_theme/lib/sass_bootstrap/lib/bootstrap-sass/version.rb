2e21f1aacd4eca2dd16c2dae0269d73cc2a9a9af
Acquia Cloud <support@acquia.com>
2015-01-22 10:07:53 +0000

    Importing site archive

---
+++ docroot/profiles/drupalaton_profile/themes/drupalaton_theme/lib/sass_bootstrap/lib/bootstrap-sass/version.rb
@@ -0,0 +1,4 @@
+module Bootstrap
+  VERSION = '3.1.1.1'
+  BOOTSTRAP_SHA = '9c4afc577253ada54d3ff27965e380a5c9f4e60e'
+end
---
+++ docroot/profiles/drupalaton_profile/themes/drupalaton_theme/template.php
@@ -0,0 +1,67 @@
+<?php 
+
+function live_oneday(){  
+  $html = '
+  <div id="live" class="oneday">
+    <div class="inner">
+      <a href="/broadcast/workshop">
+        <div class="icon"></div>
+        <div class="text">
+            <div class="day">
+              FRIDAY
+            </div>
+            <div class="dayname">
+              WORKSHOP DAY
+            </div>
+        </div>  
+      </a>
+    </div>  
+  </div>';
+  return $html;
+}
+
+function live_twoday(){  
+  $html = '<div id="live" class="twoday">
+    <div class="inner">
+      <a href="/broadcast/session-a">
+        <div class="icon"></div>
+        <div class="text">
+          <div class="day">
+            SATURDAY
+          </div>
+          <div class="dayname">
+            SESSIONS A
+          </div>
+        </div>  
+      </a>
+      <a href="/broadcast/session-b">
+        <div class="icon"></div>
+        <div class="text">
+          <div class="day">
+            SATURDAY
+          </div>
+          <div class="dayname">
+            SESSIONS B
+          </div>
+        </div>  
+      </a>
+    </div>  
+  </div>';	
+  return $html;
+}
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function drupalaton_theme_preprocess_html(&$variables) {
+  $variables['path_to_theme'] = url(drupal_get_path('theme', 'drupalaton_theme', array('absolute' => TRUE)));
+}
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function drupalaton_theme_preprocess_page(&$variables) {
+  $variables['sponsor_diamond'] = views_embed_view('sponsors', 'block_1');
+  $variables['sponsor_diamond_header'] = views_embed_view('sponsors', 'sponsor_diamond_header');
+  $variables['sponsor_gold'] = views_embed_view('sponsors', 'block_2');
+}
