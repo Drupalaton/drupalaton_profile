2a0e753c9cd91e541ef26b1c1fddf449e259219a
David Segesvari <segidu@gmail.com>
2015-02-01 00:51:25 +0100

    Test again

--- hooks/dev/post-code-update/hello-world.sh
+++ hooks/dev/post-code-update/hello-world.sh
@@ -5,7 +5,7 @@
 
 site="$1"
 target_env="$2"
-drush @$site.$target_env status --fields=root --field-labels=0
+drush @$site.$target_env status
 echo "Path:$root_directory/profiles/drupalaton/drupalaton_profile/drupalaton_profile.make"
 
 #drush @$site.$target_env make "$root_directory/profiles/drupalaton_profile/drupalaton_profile.make" --yes
