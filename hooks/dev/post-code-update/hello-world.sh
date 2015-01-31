42054afd8d36189f1864d789aa9887d6fbe02398
David Segesvari <segidu@gmail.com>
2015-02-01 00:41:55 +0100

    Test again

--- hooks/dev/post-code-update/hello-world.sh
+++ hooks/dev/post-code-update/hello-world.sh
@@ -6,6 +6,7 @@
 site="$1"
 target_env="$2"
 root_directory=$(drush @$site.$target_env status --fields=root --field-labels=0 | tr -d ' ')
+echo "Path:$root_directory/profiles/drupalaton/drupalaton_profile/drupalaton_profile.make"
 
 drush @$site.$target_env make "$root_directory/profiles/drupalaton_profile/drupalaton_profile.make" --yes
 
