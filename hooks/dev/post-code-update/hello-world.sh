6ed137f50236a2c696379dbf864b0362c6148aa5
David Segesvari <segidu@gmail.com>
2015-02-01 22:16:25 +0100

    Update the code update script

--- hooks/dev/post-code-update/hello-world.sh
+++ hooks/dev/post-code-update/hello-world.sh
@@ -5,7 +5,5 @@
 
 site="$1"
 target_env="$2"
-#site_url="http://drupalatondev.prod.acquia-sites.com"
-echo "PWD: $(pwd)"
-drush @$site.$target_env make "$site_url/profiles/drupalaton_profile/drupalaton_profile.make" --yes
+drush @$site.$target_env make /var/www/html/drupalatondev --yes
 
